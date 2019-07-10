1086abdfbd4bcb91ae09adbf7e057d5cf4149441
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-10 16:33:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  family = ["Rachel", "David", "Jane", "Mark"]
+  ["Rachel", "David", "Jane", "Mark"]
 end
 
 def add_element_to_end_of_array(array, element)
-  family << "Ruth"
+  << "Ruth"
 end
 
 def add_element_to_start_of_array(array, element)
