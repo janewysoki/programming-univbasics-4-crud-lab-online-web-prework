39aa9d87a901d939a27403262c2455615c2059fe
Jane Wysoki <jane.wysoki@gmail.com>
2019-07-10 16:34:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- 
+  family = ["Rachel", "David", "Jane", "Mark"]
+  family << "Ruth"
 end
 
 def add_element_to_start_of_array(array, element)
